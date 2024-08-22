4c20e37206ce8dd687308508f18198e4acf48a35
mrityunjay kumar <learnermrityunjay@gmail.com>
2024-08-23 00:57:16 +0530

    Error Handling added

--- app.js
+++ app.js
@@ -1,13 +1,32 @@
 const express= require('express');
+const morgan = require('morgan');
 
 const app = express();
+
 
 
 const productRoutes= require('./api/routes/product')
 const orderRoutes= require('./api/routes/orders')
 
+app.use(morgan('dev'));  // logger dependeny
+
 app.use('/products',productRoutes);  
 app.use('/orders',orderRoutes);
+
+app.use((req,res,next)=>{
+    const error = new Error('Not Found');
+    error.status = 404;
+    next(error);
+})
+
+app.use((error,req,res,next)=>{
+    res.status(error.status||500);
+    res.json({
+        error:{
+            message:error.message
+        }
+    })
+})
 
 // app.use((req,res,next)=>{
 //     res.status(200).json({
