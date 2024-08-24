cca3078830291e13365f892bb6a38f2439d8e625
mrityunjay kumar <learnermrityunjay@gmail.com>
2024-08-24 22:58:48 +0530

    order Post added

--- api/routes/orders.js
+++ api/routes/orders.js
@@ -10,9 +10,14 @@ router.get('/',(req,res,next)=>{
 
 router.post('/',(req,res,next)=>{
 
+    const order = {
+        productId:req.body.productId,
+        quantity:req.body.quantity
+    }
     
     res.status(200).json({
-        message:'this is orders POST request handeling'
+        message:'this is orders POST request handeling',
+        orderDetails:order
     })
 });
 
